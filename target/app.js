93b1956ed6a1819328f426574e25f32332d725c6
Tom Richter <tomrichter99@gmail.com>
2021-07-24 19:29:28 +0200

    add log

--- target/app.js
+++ target/app.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const app = express()
 const port = 3000
 
+console.log('PGCON', process.env.PGCONNECTION)
 const client = new Client({ connectionString: process.env.PGCONNECTION })
 client.connect()
 
